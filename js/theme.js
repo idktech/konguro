b40e06056bdddd6b4ee8091b095b62b9c9ff76b3
idktech <idk@gmx.com>
2018-03-04 11:13:44 +0200

    js updated video link idktech

--- js/theme.js
+++ js/theme.js
@@ -280,3 +280,24 @@ $(function() {
         }
     })
 });
+
+// idktech video script
+$('.video').magnificPopup({
+    type: 'iframe',
+    
+    
+    iframe: {
+       markup: '<div class="mfp-iframe-scaler">'+
+                  '<div class="mfp-close"></div>'+
+                  '<iframe width="853" height="480" src="https://www.youtube.com/embed/n1rwmodhdl4?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'+
+                  '<div class="mfp-title">Some caption</div>'+
+                '</div>'
+    },
+    callbacks: {
+      markupParse: function(template, values, item) {
+       values.title = item.el.attr('title');
+      }
+    }
+    
+    
+  });
